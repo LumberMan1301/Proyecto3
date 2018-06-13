b0d378c1346a96fd38e4468a469053674cdd58f8
Mariano <marianomm@rocketmail.com>
2018-06-12 21:28:46 -0600

    21.28
    ya lee lo que hay en el Jar

--- src/proyecto3/manejadorArchivos/manejadorArchivos.java
+++ src/proyecto3/manejadorArchivos/manejadorArchivos.java
@@ -6,11 +6,17 @@
 package proyecto3.manejadorArchivos;
 
 
+
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import proyecto3.interfaz.Principal.pantallaP;
 
 
 /**
@@ -20,13 +26,29 @@ import java.util.logging.Logger;
 public class manejadorArchivos {
     public static void leerArchivo(String ruta){
         try {
-            JarFile jf = new JarFile(ruta);//ruta es el path del jar
-            System.out.println(jf.MANIFEST_NAME.toCharArray());
+            ZipFile jf = new JarFile(ruta);//ruta es el path del jar    
+            System.out.println(jf.getName()); 
+            Enumeration<? extends ZipEntry> entries = jf.entries();
+
+            while(entries.hasMoreElements()){
+                ZipEntry entry = entries.nextElement();
+                if(entry.isDirectory()){
+                    System.out.println("dir  : " + entry.getName());
+                    pantallaP.llenarCont("dir  : " + entry.getName()+"\n");
+                } else {
+                    System.out.println("file : " + entry.getName());
+                    pantallaP.llenarCont1("file : " + entry.getName()+"\n");
+                }
+           }
            
         } catch (IOException ex) {
             Logger.getLogger(manejadorArchivos.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        
     
     }
+    
+    
     
 }
